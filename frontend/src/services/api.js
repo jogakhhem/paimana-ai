92bc11d7fba964ddb4d8baeca2cda2be6e7828c8
jogakhhem <gogoijogakhhemjyoti@gmail.com>
2026-09-10 19:00:11 +0530

    Connect frontend to Render API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -84,8 +84,11 @@
 
 import axios from "axios";
 
+// const API = axios.create({
+//   baseURL: "http://127.0.0.1:8000",
+// });
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 
