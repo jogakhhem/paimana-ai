012f49cab4745ec636846ca0a83292dbd04c2e25
jogakhhem <gogoijogakhhemjyoti@gmail.com>
2026-09-10 19:49:14 +0530

    Fix FastAPI CORS

--- backend/main.py
+++ backend/main.py
@@ -214,12 +214,13 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://paimana-ai-five.vercel.app"
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
-    allow_credentials=True,
+    # allow_origins=[
+    #     "https://paimana-ai-five.vercel.app"
+    #     "http://localhost:5173",
+    #     "http://127.0.0.1:5173",
+    # ],
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
